ca905b3f3f2b852626fcb767ff9e689f98ed58e1
Cam White (Jex) <72230943+cmrxnn@users.noreply.github.com>
2022-02-07 22:44:52 +0000

    Add basic file structure

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Println("[DAEMON] system time is" + time.Now().Format(time.RFC850))
+
+	app := fiber.New()
+
+	fmt.Println("[DAEMON] app started on port 3000")
+
+	app.Static("/", "./public/index.html")
+	app.Static("*", "./public/error.html")
+
+	app.Listen(":3000")
+}

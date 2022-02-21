380e846f0dc87529240d8ee47b0096f29f6a2dff
Cam White <72230943+cmrxnn@users.noreply.github.com>
2022-02-21 22:30:43 +0000

    Update TOS links, update compare links, beautify 404 page

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
+// -------------------- //
+// nero-nodes/site      //
+// /main.go             //
+// (c) 2022 Nero        //
+// http://neronodes.net //
+// -------------------- //
+
 package main
 
+// Import our libraries needed for this project.
 import (
 	"fmt"
 	"time"
@@ -7,16 +15,22 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// Create a main function to start the webserver instance.
 func main() {
-	fmt.Println("[DAEMON] system time is" + time.Now().Format(time.RFC850))
+	// Print system time to console for debug purposes.
+	fmt.Println("[DAEMON] system time is " + time.Now().Format(time.RFC850))
 
+	// Create the Fiber webserver instance.
 	app := fiber.New()
 
-	fmt.Println("[DAEMON] app started on port 3000")
-
+	// Create the routes for pages and assets.
 	app.Static("/assets", "./assets")
 	app.Static("/", "./public/index.html")
 	app.Static("*", "./public/error.html")
 
+	// Listen on port 3000 for incoming requests.
 	app.Listen(":3000")
+
+	// Log this event to the console.
+	fmt.Println("[DAEMON] app started on port 3000")
 }
